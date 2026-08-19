8981262bfc9b3826348ec204fa69a5619ec1d0d2
François Basset <mrfrancoisbasset@gmail.com>
2026-08-19 10:03:25 +0200

    PokéCross: add native WebView activity

---
+++ pokecross/app/src/main/java/fr/francoisbasset/pokecross/MainActivity.java
@@ -0,0 +1,58 @@
+package fr.francoisbasset.pokecross;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setStatusBarColor(Color.rgb(11, 16, 32));
+        getWindow().setNavigationBarColor(Color.rgb(11, 16, 32));
+
+        webView = new WebView(this);
+        webView.setBackgroundColor(Color.rgb(11, 16, 32));
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setTextZoom(100);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        setContentView(webView);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.destroy();
+            webView = null;
+        }
+        super.onDestroy();
+    }
+}
